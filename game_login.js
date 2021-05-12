22ff3413cf344f4e4592e8ed5939214aabbce023
AnushkaSinha2020 <75123533+AnushkaSinha2020@users.noreply.github.com>
2021-05-12 18:50:20 +0530

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -76,4 +76,5 @@ function check(){
         answer_turn= "player1"
         document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name;
     }
+    document.getElementById("output").innerHTML= "";
 }
